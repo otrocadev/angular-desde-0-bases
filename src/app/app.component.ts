d294cde2eb8ff304c158f87672ed4ef9f9856808
Ot Roca <otrocadev@gmail.com>
2026-04-08 17:35:54 +0200

    add navbar component with routing links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'Fernando';
+  title = 'Ot';
 }
