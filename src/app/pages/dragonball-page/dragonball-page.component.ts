71ce001486aaf379e9fb21cda613d673bab4dc42
Ot Roca <otrocadev@gmail.com>
2026-04-08 18:30:27 +0200

    add dragonball page component

---
+++ src/app/pages/dragonball-page/dragonball-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dragonball-page',
+  imports: [],
+  templateUrl: './dragonball-page.component.html',
+})
+export class DragonballPageComponent {}
