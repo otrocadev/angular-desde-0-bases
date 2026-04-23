0b37c52f12556d18672fd3f6f04ab1ac082f1aa8
Ot Roca <ot.roca@col.vueling.com>
2026-04-23 21:27:56 +0200

    for- iteraciones de elementos

--- src/app/pages/dragonball-page/dragonball-page.component.ts
+++ src/app/pages/dragonball-page/dragonball-page.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
 
 @Component({
   selector: 'app-dragonball-page',
   imports: [],
   templateUrl: './dragonball-page.component.html',
 })
-export class DragonballPageComponent {}
+export class DragonballPageComponent {
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 9001 },
+    { id: 2, name: 'Vegetta', power: 8000 },
+    { id: 3, name: 'Piccolo', power: 7000 },
+  ]);
+}
