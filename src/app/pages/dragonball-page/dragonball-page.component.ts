1ebc027272add99ec14618e23ca9a52c6e2f3404
Ot Roca <ot.roca@col.vueling.com>
2026-04-23 21:39:21 +0200

    if

--- src/app/pages/dragonball-page/dragonball-page.component.ts
+++ src/app/pages/dragonball-page/dragonball-page.component.ts
@@ -16,5 +16,6 @@ export class DragonballPageComponent {
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegetta', power: 8000 },
     { id: 3, name: 'Piccolo', power: 7000 },
+    { id: 4, name: 'Yamcha', power: 500 },
   ]);
 }
